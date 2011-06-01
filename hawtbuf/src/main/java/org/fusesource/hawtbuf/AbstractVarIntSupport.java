ab9b0c101a4b62fa1367d155730337d02b661534
Hiram Chirino <hiram@hiramchirino.com>
2011-06-01 05:39:12 -0400

    Added static helpers for computing the size of a var encoded int/long

--- hawtbuf/src/main/java/org/fusesource/hawtbuf/AbstractVarIntSupport.java
+++ hawtbuf/src/main/java/org/fusesource/hawtbuf/AbstractVarIntSupport.java
@@ -135,4 +135,57 @@ abstract public class AbstractVarIntSupport {
         return (n << 1) ^ (n >> 63);
     }
 
+    /**
+     * Compute the number of bytes that would be needed to encode a varint.
+     * {@code value} is treated as unsigned, so it won't be sign-extended if
+     * negative.
+     */
+    public static int computeVarIntSize(int value) {
+        if ((value & (0xffffffff << 7)) == 0)
+            return 1;
+        if ((value & (0xffffffff << 14)) == 0)
+            return 2;
+        if ((value & (0xffffffff << 21)) == 0)
+            return 3;
+        if ((value & (0xffffffff << 28)) == 0)
+            return 4;
+        return 5;
+    }
+
+    /** Compute the number of bytes that would be needed to encode a varint. */
+    public static int computeVarLongSize(long value) {
+        if ((value & (0xffffffffffffffffL << 7)) == 0)
+            return 1;
+        if ((value & (0xffffffffffffffffL << 14)) == 0)
+            return 2;
+        if ((value & (0xffffffffffffffffL << 21)) == 0)
+            return 3;
+        if ((value & (0xffffffffffffffffL << 28)) == 0)
+            return 4;
+        if ((value & (0xffffffffffffffffL << 35)) == 0)
+            return 5;
+        if ((value & (0xffffffffffffffffL << 42)) == 0)
+            return 6;
+        if ((value & (0xffffffffffffffffL << 49)) == 0)
+            return 7;
+        if ((value & (0xffffffffffffffffL << 56)) == 0)
+            return 8;
+        if ((value & (0xffffffffffffffffL << 63)) == 0)
+            return 9;
+        return 10;
+    }
+
+    /**
+     * Compute the number of bytes that would be needed to encode a signed varint.
+     */
+    public static int computeVarSignedIntSize(int value) {
+        return computeVarIntSize(encodeZigZag32(value));
+    }
+
+    /**
+     * Compute the number of bytes that would be needed to encode a signed varint.
+     */
+    public static int computeVarSignedLongSize(long value) {
+        return computeVarLongSize(encodeZigZag64(value));
+    }
 }
